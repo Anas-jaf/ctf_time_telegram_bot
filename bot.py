738756faac8c6777c097af59178ce3a9fc1c3e3c
Anas-jaf <ansas4565@gmail.com>
2023-07-13 02:34:00 +0300

    انتهيت من المشروع وحققت الهدف

--- bot.py
+++ bot.py
@@ -1,36 +1,59 @@
 #! /user/bin/python3
 from telegram import Bot, Update
+from ctf_times_utils import *
 from telegram.ext import *
 from keys import token as bot_token
+import traceback
 import datetime
 import pytz
 
 print('Starting up bot...')
 
-group_chat_id = '-901580361'
+# group_chat_id = '-901580361'
+group_chat_id ='-1001904108964'
 
+commands_msg = '''
+/start امر لتشغيل البوت
+/commands الاوامر المتوفرة في البوت
+/upcomming_CTF مسابقات امسك العلم القادمة في الخمس ايام القادمة
+/help عرض المساعدة 
+'''
+
+# Log errors
+def error(update, context):
+    traceback.print_exc()    
+    print(f'Update {update} caused error {context.error}')
+
+# Lets us use the /help command
+def help_command(update, context):
+    update.message.reply_text(commands_msg)
+
+def upcomming_CTF(update, context):
+    for event in incomming_events_list_wrapper():
+        update.message.reply_text('\n'.join([part for part in event])) 
+
+def commands(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=commands_msg)
 
 def send_message(message):
     bot = Bot(token=bot_token)
-    bot.send_message(chat_id=group_chat_id, text=message)
-
+    # bot.send_message(chat_id=group_chat_id, text=message)
+    for event in incomming_events_list_wrapper():
+        bot.send_message(chat_id=group_chat_id, text='\n'.join([part for part in event]) ,timeout=900)
 
 def start_of_day_message():
     message = "Good morning! Have a wonderful day ahead!"
     send_message(message)
 
-
 def middle_of_day_message():
     message = "Hello! Just a friendly reminder to take a break and relax!"
     send_message(message)
-
 
 def end_of_day_message():
     message = "Good evening! Hope you had a productive day. Have a restful night!"
     send_message(message)
 
-
-def start_command(update: Update, context):
+def start_command(update, context):
     message = "Bot is started. Messages will be sent at specific times."
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
@@ -38,8 +61,14 @@ def main():
     updater = Updater(bot_token, use_context=True)
     dp = updater.dispatcher
 
+    # Log all errors
+    dp.add_error_handler(error)
+    
     # Commands
     dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CommandHandler('commands', commands))
+    dp.add_handler(CommandHandler('upcomming_CTF', upcomming_CTF))
+    dp.add_handler(CommandHandler('help', help_command))
 
     # Set the timezone to GMT+03:00
     timezone = pytz.timezone('Asia/Amman')
@@ -48,21 +77,20 @@ def main():
     job_queue = updater.job_queue
 
     # Define the message times in GMT+03:00 timezone
-    start_time = datetime.time(hour=6, minute=0, tzinfo=timezone)
-    middle_time = datetime.time(hour=10, minute=0, tzinfo=timezone)
-    end_time = datetime.time(hour=21, minute=54, tzinfo=timezone)
+    start_time = datetime.time(hour=7, minute=0, tzinfo=timezone)
+    # middle_time = datetime.time(hour=13, minute=0, tzinfo=timezone)
+    # end_time = datetime.time(hour=21, minute=0, tzinfo=timezone)
 
 
     # Schedule the messages using run_daily method
     job_queue.run_daily(lambda context: start_of_day_message(), start_time, context=group_chat_id)
-    job_queue.run_daily(lambda context: middle_of_day_message(), middle_time, context=group_chat_id)
-    job_queue.run_daily(lambda context: end_of_day_message(), end_time, context=group_chat_id)
+    # job_queue.run_daily(lambda context: middle_of_day_message(), middle_time, context=group_chat_id)
+    # job_queue.run_daily(lambda context: end_of_day_message(), end_time, context=group_chat_id)
 
     
     # Start the bot
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
--- ctf_times_utils.py
+++ ctf_times_utils.py
@@ -2,12 +2,57 @@
 import requests 
 from pprint import pprint
 import datetime
+import json
+import pytz
+
+def incomming_events_list_wrapper(start_timestamp=None,finish_timestamp=None,limit=100,session=None):
+    
+    return [
+                [  
+                    "TITLE : " + i['title'],
+                    "ORGANIZER : " + i['organizers'][0]['name'][:30] + ' ...' if len(i['organizers'][0]['name']) else '',
+                    "STARTS AT : " + format_time(i['start']),
+                    "ENDS AT : " + format_time(i['finish']),
+                    "DESCRIPTION : " + i['description'][:30].replace('\n','\\n').replace('\r','\\r')+' ...',
+                    "URL LINK : " + i['url'],
+                    "CTF TYPE : " + i['format'],
+                    "PARTICIPANT NUMBER : " + str(i['participants']),
+                    "DURATION : " +json.dumps(i['duration'])
+                ] 
+                for i in get_incomming_events(start_timestamp=start_timestamp,finish_timestamp=finish_timestamp,limit=limit,session=session)
+            ]
+    
+def format_time(time_string):
+    # Convert string to datetime object
+    datetime_obj = datetime.datetime.fromisoformat(time_string)
+
+    # Create a new naive datetime object without time zone information
+    naive_datetime = datetime.datetime(datetime_obj.year, datetime_obj.month, datetime_obj.day, datetime_obj.hour, datetime_obj.minute, datetime_obj.second)
+
+    # Define UTC timezone
+    utc_timezone = pytz.timezone('UTC')
+
+    # Define Asia/Amman timezone
+    amman_timezone = pytz.timezone('Asia/Amman')
+
+    # Convert datetime to UTC timezone
+    datetime_utc = utc_timezone.localize(naive_datetime)
+
+    # Convert UTC datetime to Asia/Amman timezone
+    datetime_amman = datetime_utc.astimezone(amman_timezone)
+
+    # Format the datetime in Asia/Amman timezone with AM/PM indication
+    formatted_time = datetime_amman.strftime("%A, %Y-%m-%d at %I:%M %p Amman")
+
+    return formatted_time
 
 def make_request(url ,session=None,timeout_seconds=60):
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0'}
     if session is None :
-        response = requests.request("GET", url, timeout=timeout_seconds)
+        response = requests.request("GET", url, timeout=timeout_seconds, headers=headers)
     else : 
-        response = session.get(url , timeout=timeout_seconds)
+        response = session.get(url , timeout=timeout_seconds, headers=headers)
         
     if "403 Forbidden" not in response.text :
         return response.json()
@@ -19,12 +64,12 @@ def get_incomming_events(start_timestamp=None,finish_timestamp=None,limit=100,se
     
     if not start_timestamp : 
         start_timestamp = default_start_end[0]
-    elif not finish_timestamp:
+    if not finish_timestamp:
         finish_timestamp = default_start_end[1]
         
-    url = 'https://ctftime.org/api/v1/events/?limit={}&start={}&finish={}'.format(limit=limit,
-                                                                                  start_timestamp=start_timestamp,
-                                                                                  finish_timestamp=finish_timestamp
+    url = 'https://ctftime.org/api/v1/events/?limit={}&start={}&finish={}'.format(limit,
+                                                                                  start_timestamp,
+                                                                                  finish_timestamp
                                                                                   )
     return make_request(url=url,session=session)
 
@@ -34,7 +79,7 @@ def timestamp_now_and_TillTime(after=5):
     after_after_days_timestamp = after_after_days.timestamp()
     
     return [current_timestamp , after_after_days_timestamp]
-        
+
 def get_running_events():
     pass
 
@@ -43,6 +88,7 @@ def get_time_stamp():
 
 def main():
     print('starting script')
+    print(get_incomming_events())
 
 if __name__ == "__main__":
     main()
